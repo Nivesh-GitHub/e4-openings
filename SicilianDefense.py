495c606c469c1631d74f70f043d9efed35acf7c4
Nivesh-GitHub <89345698+Nivesh-GitHub@users.noreply.github.com>
2021-09-11 08:52:53 +0530

    Create SicilianDefense.py

---
+++ SicilianDefense.py
@@ -0,0 +1,9 @@
+#Sicilian Defense
+board = chess.Board()
+board.push_san("e4") #It will push the piece to e4
+board.push_san("c5")
+
+
+
+
+board #instead of print you can do the board this is compulsary else you will not be able to see the board
